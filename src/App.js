9d791e83b8f9dc9338856d07e04b80cb0d36fc44
Hamilton <>
2023-06-22 13:32:39 +0200

    added multiple face recognition

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 const initialState = {
   input: '',
   imageUrl: '',
-  box: {},
+  boxes: [],
   route: 'signin',
   isSignedIn: false,
   user: {
@@ -42,20 +42,24 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace = data.outputs[0].data.regions;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+    let boxes = [];
+    for (let i = 0; i < clarifaiFace.length; i++) {
+      boxes.push({
+        leftCol: clarifaiFace[i].region_info.bounding_box.left_col * width,
+        topRow: clarifaiFace[i].region_info.bounding_box.top_row * height,
+        rightCol: width - (clarifaiFace[i].region_info.bounding_box.right_col * width),
+        bottomRow: height - (clarifaiFace[i].region_info.bounding_box.bottom_row * height)
+      })
     }
+    return boxes;
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+  displayFaceBox = (boxes) => {
+    this.setState({boxes: boxes});
   }
 
   onInputChange = (event) => {
@@ -103,7 +107,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
         <ParticlesBg type="circle" bg={true} />
@@ -119,7 +123,7 @@ class App extends Component {
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
+              <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
             </div>
           : (
              route === 'signin'
